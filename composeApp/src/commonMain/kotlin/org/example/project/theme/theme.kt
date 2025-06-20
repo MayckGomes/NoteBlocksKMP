52b45fdc4bafec49ba827d6eeec47e2afd590e75
Mayck Gomes <mayck.gomes2901@gmail.com>
2025-06-20 16:02:37 -0300

    feature: adicionado foto do icone do app, tanto normal, tanto adaptativo

--- composeApp/src/commonMain/kotlin/org/example/project/theme/theme.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/theme/theme.kt
@@ -7,12 +7,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val backgroundDark = Color(0xFF0E0E0E)
-private val backgroundLight = Color(0xFFF0F0F0)
-
-private val primaryColor = Color(0xFF0081ff)
-private val secunderyColor = Color(0xff005db8)
-
 @Composable
 fun MyAppTheme(
     content : @Composable () -> Unit
